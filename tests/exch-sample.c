cc9c0786d5d51c2f1b2660aa4539fd1a79146c4d
lysannkessler <lysann.kessler@gmail.com>
2012-08-13 12:35:25 +0200

    refactor exch-sample

--- tests/exch-sample.c
+++ tests/exch-sample.c
@@ -9,24 +9,35 @@ static void check_error(int result, char* msg) {
   exit(EXIT_FAILURE);
 }
 
-int main(int argc, char ** argv) {
-  struct mailexch* exch;
-  int result;
+mailexch_result list_items(mailexch* exch, mailexch_distinguished_folder_id folder_id) {
+  carray* items = NULL;
+  mailexch_result result = mailexch_list(exch, folder_id, NULL, 10, &items);
+  if(result != MAILEXCH_NO_ERROR) return result;
 
-  /*
-  ./exch-sample myhpiemail myhpiaccount myhpipassword
-  */
+  unsigned int i;
+  for(i = 0; i < items->len; i++) {
+    mailexch_type_item* item = carray_get(items, i);
+    printf("  %s\n", item->subject);
+  }
+  mailexch_type_item_array_free(items);
+
+  return result;
+}
+
+int main(int argc, char ** argv) {
+  /* ./exch-sample myhpiemail myhpiaccount myhpipassword */
   if (argc < 4) {
     fprintf(stderr, "usage: exch-sample [HPI email address] [HPI user name] [password]\n");
     exit(EXIT_FAILURE);
   }
 
-  exch = mailexch_new(0, NULL);
+  mailexch* exch = mailexch_new(0, NULL);
   if(exch == NULL) {
     fprintf(stderr, "Could not create mailexch instance.");
     exit(EXIT_FAILURE);
   }
 
+  mailexch_result result;
 #if 1
   mailexch_connection_settings settings;
   memset(&settings, 0, sizeof(settings));
@@ -39,24 +50,12 @@ int main(int argc, char ** argv) {
   check_error(result, "could not connect");
 
   puts("INBOX");
-  carray* items = NULL;
-  result = mailexch_list(exch, MAILEXCH_DISTFOLDER_INBOX, NULL, 10, &items);
+  result = list_items(exch, MAILEXCH_DISTFOLDER_INBOX);
   check_error(result, "could not list items in inbox");
-  unsigned int i;
-  for(i = 0; i < items->len; i++) {
-    mailexch_type_item* item = carray_get(items, i);
-    printf("  %s\n", item->subject);
-  }
-  mailexch_type_item_array_free(items); items = NULL;
 
   puts("SENT ITEMS");
-  result = mailexch_list(exch, MAILEXCH_DISTFOLDER_SENTITEMS, NULL, 10, &items);
+  result = list_items(exch, MAILEXCH_DISTFOLDER_SENTITEMS);
   check_error(result, "could not list sent items");
-  for(i = 0; i < items->len; i++) {
-    mailexch_type_item* item = carray_get(items, i);
-    printf("  %s\n", item->subject);
-  }
-  mailexch_type_item_array_free(items); items = NULL;
 
   mailexch_free(exch);
 
