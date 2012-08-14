1bbec6a8fd79bb32f19fdd797a38f6c48541bece
lysannkessler <lysann.kessler@gmail.com>
2012-08-14 14:51:00 +0200

    clean up oxws-sample program

--- tests/oxws-sample.c
+++ tests/oxws-sample.c
@@ -3,33 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void check_error(int result, char* msg) {
-  if (result == OXWS_NO_ERROR) return;
-  fprintf(stderr, "%s. result: %d\n", msg, result);
-  exit(EXIT_FAILURE);
-}
 
-oxws_result list_items(oxws* oxws, oxws_distinguished_folder_id folder_id) {
-  carray* items = NULL;
-  oxws_result result = oxws_list(oxws, folder_id, NULL, 10, &items);
-  if(result != OXWS_NO_ERROR) return result;
+oxws_result list_items(oxws* oxws, oxws_distinguished_folder_id folder_id);
+static void check_error(int result, char* msg);
 
-  unsigned int i;
-  for(i = 0; i < items->len; i++) {
-    oxws_type_item* item = carray_get(items, i);
-    printf("  %s\n", item->subject);
-  }
-  oxws_type_item_array_free(items);
-
-  return result;
-}
-
-void progress_callback(size_t count, size_t total, void* userdata) {
-  (void)(userdata); /* unused */
-  if(total > 0) {
-    printf("(received %.0lf%%)\n", (double)count * 100.0 / (double)total);
-  }
-}
 
 int main(int argc, char ** argv) {
   /* ./oxws-sample myhpiemail myhpiaccount myhpipassword */
@@ -38,13 +15,14 @@ int main(int argc, char ** argv) {
     exit(EXIT_FAILURE);
   }
 
+  /* create instance */
   oxws* oxws = oxws_new();
   if(oxws == NULL) {
     fprintf(stderr, "Could not create oxws instance.");
     exit(EXIT_FAILURE);
   }
-  oxws_set_progress_callback(oxws, progress_callback, NULL, 4096);
 
+  /* configure connection settings */
   oxws_result result;
 #if 1
   oxws_connection_settings settings;
@@ -54,18 +32,44 @@ int main(int argc, char ** argv) {
 #else
   result = oxws_autodiscover_connection_settings(oxws, "owa2.hpi.uni-potsdam.de", argv[1], argv[2], argv[3], NULL);
 #endif
+
+  /* connect */
   result = oxws_connect(oxws, argv[2], argv[3], NULL);
   check_error(result, "could not connect");
 
+  /* list items in Inbox */
   puts("INBOX");
   result = list_items(oxws, OXWS_DISTFOLDER_INBOX);
   check_error(result, "could not list items in inbox");
 
-  puts("SENT ITEMS");
-  result = list_items(oxws, OXWS_DISTFOLDER_SENTITEMS);
-  check_error(result, "could not list sent items");
-
+  /* cleanup */
   oxws_free(oxws);
-
   return 0;
+}
+
+oxws_result list_items(oxws* oxws, oxws_distinguished_folder_id folder_id) {
+  /* perform request */
+  carray* items = NULL;
+  oxws_result result = oxws_list(oxws, folder_id, NULL, 10, &items);
+  if(result != OXWS_NO_ERROR) return result;
+
+  /* list all items */
+  unsigned int i;
+  for(i = 0; i < items->len; i++) {
+    oxws_type_item* item = carray_get(items, i);
+    printf("  %s\n", item->subject);
+  }
+
+  /* clean up */
+  oxws_type_item_array_free(items);
+  return result;
+}
+
+static void check_error(int result, char* msg) {
+  if (result == OXWS_NO_ERROR)
+    return;
+
+  /* print error and exit */
+  fprintf(stderr, "%s. result: %d\n", msg, result);
+  exit(EXIT_FAILURE);
 }
