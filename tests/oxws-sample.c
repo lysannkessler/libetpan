7b9b2efebf834cd6979f836d2ba70d2c8e7cbb2d
lysannkessler <lysann.kessler@gmail.com>
2012-08-24 20:28:34 +0200

    oxws-sample parses command-line options

--- tests/oxws-sample.c
+++ tests/oxws-sample.c
@@ -1,17 +1,43 @@
-#include <libetpan/libetpan.h>
+#define _GNU_SOURCE
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef _MSC_VER
+# include "../src/bsd/getopt.h"
+#else
+# include <getopt.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
 
 #include <stdlib.h>
 #include <string.h>
 
+#include <libetpan/libetpan.h>
 
+
+int parse_options(int argc, char** argv,
+      char** user, char** host, char** email, char** password,
+      char** ews_url, short* send);
+void print_usage();
 oxws_result list_items(oxws* oxws, oxws_distinguished_folder_id folder_id);
 static void check_error(oxws_result result, char* msg);
 
 
 int main(int argc, char ** argv) {
-  /* ./oxws-sample myhpiemail myhpiaccount myhpipassword */
-  if (argc < 4) {
-    fprintf(stderr, "usage: oxws-sample [HPI email address] [HPI user name] [password]\n");
+  /* parse options */
+  char *user, *host, *email, *password, *ews_url;
+  short send;
+  int r = parse_options(argc, argv, &user, &host, &email, &password, &ews_url, &send);
+  if(r < 0) {
+    fprintf(stderr, "parse_options: internal error\n");
+    exit(EXIT_FAILURE);
+  } else if(r > 0) {
+    print_usage();
     exit(EXIT_FAILURE);
   }
 
@@ -24,52 +50,163 @@ int main(int argc, char ** argv) {
 
   /* configure connection settings */
   oxws_result result;
-#if 1
-  oxws_connection_settings settings;
-  memset(&settings, 0, sizeof(settings));
-  settings.as_url = "https://owa2.hpi.uni-potsdam.de/EWS/Exchange.asmx";
-  result = oxws_set_connection_settings(oxws, &settings);
-#else
-  result = oxws_autodiscover_connection_settings(oxws, "owa2.hpi.uni-potsdam.de", argv[1], argv[2], argv[3], NULL);
-#endif
+  if(ews_url) {
+    oxws_connection_settings settings;
+    memset(&settings, 0, sizeof(settings));
+    settings.as_url = ews_url;
+    result = oxws_set_connection_settings(oxws, &settings);
+  } else {
+    result = oxws_autodiscover_connection_settings(oxws, host, user, password, email, NULL);
+  }
 
   /* connect */
-  result = oxws_connect(oxws, argv[2], argv[3], NULL);
+  result = oxws_connect(oxws, user, password, NULL);
   check_error(result, "could not connect");
 
-#if 1
   /* list items in Inbox */
   puts("INBOX");
   result = list_items(oxws, OXWS_DISTFOLDER_INBOX);
   check_error(result, "could not list items in inbox");
-#endif
 
-#if 0
-  /* send message and save it to Sent Items */
-  oxws_message* message = oxws_message_new();
-  oxws_item_set_subject_cstring((oxws_item*) message, "[libetpan test] message from oxws-sample");
-  oxws_item_set_body_fields_cstring((oxws_item*) message,
-          "This is just another email sent using libetpan's Exchange implementation.\n" \
-          "It's ok to feel annoyed by now :P",
-          OXWS_BODY_TYPE_TEXT);
-  message->to_recipients = carray_new(3);
-  oxws_email_address* address = oxws_email_address_new();
-  oxws_email_address_set_email_address(address, argv[1]);
-  carray_add(message->to_recipients, address, NULL);
-  oxws_create_item(oxws, (oxws_item*) message, OXWS_MESSAGE_DISPOSITION_SEND_AND_SAVE_COPY, OXWS_DISTFOLDER_SENTITEMS, NULL);
-#endif
+  if(send) {
+    /* send message and save it to Sent Items */
+    oxws_message* message = oxws_message_new();
+    oxws_item_set_subject_cstring((oxws_item*) message, "[libetpan test] message from oxws-sample");
+    oxws_item_set_body_fields_cstring((oxws_item*) message,
+            "This is just another email sent using libetpan's Exchange implementation.\n" \
+            "It's ok to feel annoyed by now :P",
+            OXWS_BODY_TYPE_TEXT);
+    message->to_recipients = carray_new(3);
+    oxws_email_address* address = oxws_email_address_new();
+    oxws_email_address_set_email_address(address, user);
+    carray_add(message->to_recipients, address, NULL);
+    oxws_create_item(oxws, (oxws_item*) message, OXWS_MESSAGE_DISPOSITION_SEND_AND_SAVE_COPY, OXWS_DISTFOLDER_SENTITEMS, NULL);
 
-#if 0
-  /* list items in Sent Items */
-  puts("SENT ITEMS");
-  result = list_items(oxws, OXWS_DISTFOLDER_SENTITEMS);
-  check_error(result, "could not list sent items");
-#endif
+    /* list items in Sent Items */
+    puts("SENT ITEMS");
+    result = list_items(oxws, OXWS_DISTFOLDER_SENTITEMS);
+    check_error(result, "could not list sent items");
+  }
 
   /* cleanup */
   oxws_free(oxws);
   return 0;
 }
+
+
+int parse_options(int argc, char** argv,
+      char** user, char** host, char** email, char** password,
+      char** ews_url, short* send) {
+
+  static const char* short_options = "u:p:h:e:x:s";
+#if HAVE_GETOPT_LONG
+  static struct option long_options[] = {
+    {"user",     1, 0, 'u'},
+    {"host",     1, 0, 'h'},
+    {"email",    1, 0, 'e'},
+    {"password", 1, 0, 'p'},
+    {"url",      1, 0, 'x'},
+    {"send",     0, 0, 's'},
+  };
+#endif
+  int r = 0;
+
+  if(user == NULL || password == NULL || host == NULL || email == NULL || \
+     ews_url == NULL || send == NULL)
+    return -1;
+
+  *user = NULL;
+  *password = NULL;
+  *host = NULL;
+  *email = NULL;
+  *ews_url = NULL;
+  *send = 0;
+
+  while (r != -1) {
+#if HAVE_GETOPT_LONG
+    r = getopt_long(argc, argv, short_options, long_options, NULL);
+#else
+    r = getopt(argc, argv, short_options);
+#endif
+    switch (r) {
+    case 'u': *user = strdup(optarg); break;
+    case 'h': *host = strdup(optarg); break;
+    case 'e': *email = strdup(optarg); break;
+
+    case 'p': *password = strdup(optarg); break;
+
+    case 'x': *ews_url = strdup(optarg); break;
+    case 's': *send = 1; break;
+
+    case -1: break;
+    default: fprintf(stderr, "error: unknown argument %c.\n", r); break;
+    }
+  }
+
+  /* check for existance of password */
+  if(*password == NULL) {
+    fprintf(stderr, "error: missing argument password.\n");
+    return 2;
+  }
+
+  /* derive user and host from email */
+  if(*user == NULL || *host == NULL) {
+    if(*email == NULL) {
+      fprintf(stderr, "error: email must be given if user or host are missing.\n");
+      return 2;
+    }
+
+    char* delim = strstr(*email, "@");
+    if(delim == NULL) {
+      fprintf(stderr, "error: missing @ in email.\n");
+      return 3;
+    }
+    /* user */
+    if(*user == NULL) {
+      size_t length = delim - *email;
+      *user = (char*) malloc(length + 1);
+      if(*user == NULL)
+        return -2;
+      memcpy(*user, *email, length);
+      *user[length] = 0;
+    }
+    /* host */
+    if(*host == NULL) {
+      if(*(delim + 1) == 0) {
+        fprintf(stderr, "error: no host found in email.\n");
+        return 3;
+      }
+      *host = delim + 1;
+    }
+  }
+
+  /* derive email from user and host */
+  if(*email == NULL) {
+    if(*user == NULL || *host == NULL) {
+      fprintf(stderr, "error: user and host must be given if email is missing.\n");
+      return 2;
+    }
+    size_t length = strlen(*user) + strlen(*host) + 1;
+    *email = (char*) malloc(length);
+    if(*email == NULL)
+      return -2;
+    sprintf(*email, "%s@%s", *user, *host);
+  }
+
+  return 0;
+}
+
+void print_usage() {
+  fprintf(stderr, "usage: oxws-sample [options]\n\n");
+  fprintf(stderr, "options:\n" \
+                  "    --user     -u [USER]    username (optional). If missing, derive from email.\n" \
+                  "    --host     -h [HOST]    host (optional). If missing, derive from email.\n" \
+                  "    --email    -e [EMAIL]   email address (optional). If missing, derive from user and host.\n\n" \
+                  "    --password -p [PWD]     password (required)\n\n" \
+                  "    --url      -x [URL]     EWS URL (optional). If missing, use autodiscover.\n" \
+                  "    --send     -s           send a test email to oneself\n");
+}
+
 
 oxws_result list_items(oxws* oxws, oxws_distinguished_folder_id folder_id) {
   /* perform request */
