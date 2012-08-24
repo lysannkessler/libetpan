d770e7df66744d878fed77a57d2997216778dc1f
lysannkessler <lysann.kessler@gmail.com>
2012-08-24 10:43:18 +0200

    enable -Werror in low-level/pop3 and fix warnings

--- src/low-level/pop3/mailpop3.c
+++ src/low-level/pop3/mailpop3.c
@@ -104,7 +104,7 @@ static void mailpop3_msg_info_tab_free(carray * msg_tab)
 
   for(i = 0 ; i < carray_count(msg_tab) ; i++) {
     struct mailpop3_msg_info * msg;
-    
+
     msg = carray_get(msg_tab, i);
     mailpop3_msg_info_free(msg);
   }
@@ -150,7 +150,7 @@ int mailpop3_get_msg_info(mailpop3 * f, unsigned int indx,
   r = mailpop3_list(f, &tab);
   if (r != MAILPOP3_NO_ERROR)
 	return r;
-  
+
   if (tab == NULL)
     return MAILPOP3_ERROR_BAD_STATE;
 
@@ -177,7 +177,7 @@ struct mailpop3_capa * mailpop3_capa_new(char * name, clist * param)
     return NULL;
   capa->cap_name = name;
   capa->cap_param = param;
-  
+
   return capa;
 }
 
@@ -204,7 +204,7 @@ mailpop3 * mailpop3_new(size_t progr_rate, progress_function * progr_fun)
 
   f->pop3_timestamp = NULL;
   f->pop3_response = NULL;
-  
+
   f->pop3_stream = NULL;
 
   f->pop3_progr_rate = progr_rate;
@@ -221,11 +221,11 @@ mailpop3 * mailpop3_new(size_t progr_rate, progress_function * progr_fun)
   f->pop3_msg_tab = NULL;
   f->pop3_deleted_count = 0;
   f->pop3_state = POP3_STATE_DISCONNECTED;
-  
+
 #ifdef USE_SASL
   f->pop3_sasl.sasl_conn = NULL;
 #endif
-  
+
   return f;
 
  free_stream_buffer:
@@ -246,7 +246,7 @@ void mailpop3_free(mailpop3 * f)
     mailsasl_unref();
   }
 #endif
-  
+
   if (f->pop3_stream)
     mailpop3_quit(f);
 
@@ -299,7 +299,7 @@ static char * mailpop3_get_timestamp(char * response)
 
   if (response == NULL)
     return NULL;
-  
+
   begin_timestamp = strchr(response, TIMESTAMP_START);
 
   end_timestamp = NULL;
@@ -308,7 +308,7 @@ static char * mailpop3_get_timestamp(char * response)
     if (end_timestamp == NULL)
       begin_timestamp = NULL;
   }
-   
+
   if (!begin_timestamp)
     return NULL;
 
@@ -319,7 +319,7 @@ static char * mailpop3_get_timestamp(char * response)
     return NULL;
   strncpy(timestamp, begin_timestamp, len_timestamp);
   timestamp[len_timestamp] = '\0';
- 
+
   return timestamp;
 }
 
@@ -365,7 +365,7 @@ int mailpop3_quit(mailpop3 * f)
   int r;
   int res;
 
-  if ((f->pop3_state != POP3_STATE_AUTHORIZATION) 
+  if ((f->pop3_state != POP3_STATE_AUTHORIZATION)
       && (f->pop3_state != POP3_STATE_TRANSACTION)) {
     res = MAILPOP3_ERROR_BAD_STATE;
     goto close;
@@ -407,9 +407,9 @@ int mailpop3_quit(mailpop3 * f)
     mailpop3_msg_info_tab_free(f->pop3_msg_tab);
     f->pop3_msg_tab = NULL;
   }
-  
+
   f->pop3_state = POP3_STATE_DISCONNECTED;
-  
+
   return res;
 }
 
@@ -468,14 +468,14 @@ int mailpop3_apop(mailpop3 * f,
   MD5Update(&md5context, (const unsigned char *) f->pop3_timestamp, strlen (f->pop3_timestamp));
   MD5Update(&md5context, (const unsigned char *) password, strlen (password));
   MD5Final(md5digest, &md5context);
-  
+
   cmd_ptr = md5string;
   for(i = 0 ; i < 16 ; i++, cmd_ptr += 2)
     snprintf(cmd_ptr, 3, "%02x", md5digest[i]);
   * cmd_ptr = 0;
-  
+
   /* send apop command */
-  
+
   snprintf(command, POP3_STRING_SIZE, "APOP %s %s\r\n", user, md5string);
   r = send_command_private(f, command, 0);
   if (r == -1)
@@ -502,9 +502,9 @@ int mailpop3_user(mailpop3 * f, const char * user)
 
   if (f->pop3_state != POP3_STATE_AUTHORIZATION)
     return MAILPOP3_ERROR_BAD_STATE;
-  
+
   /* send user command */
-    
+
   snprintf(command, POP3_STRING_SIZE, "USER %s\r\n", user);
   r = send_command_private(f, command, 0);
   if (r == -1)
@@ -568,7 +568,7 @@ static int mailpop3_do_uidl(mailpop3 * f, carray * msg_tab)
     return MAILPOP3_ERROR_BAD_STATE;
 
   /* send list command */
-  
+
   snprintf(command, POP3_STRING_SIZE, "UIDL\r\n");
   r = send_command(f, command);
   if (r == -1)
@@ -581,7 +581,7 @@ static int mailpop3_do_uidl(mailpop3 * f, carray * msg_tab)
 
   if (r != RESPONSE_OK)
     return MAILPOP3_ERROR_CANT_LIST;
-  
+
   r = read_uidl(f, msg_tab);
   if (r != MAILPOP3_NO_ERROR)
     return r;
@@ -620,14 +620,14 @@ static int mailpop3_do_list(mailpop3 * f)
 
   if (r != RESPONSE_OK)
     return MAILPOP3_ERROR_CANT_LIST;
-  
+
   r = read_list(f, &msg_tab);
   if (r != MAILPOP3_NO_ERROR)
     return r;
 
   f->pop3_msg_tab = msg_tab;
   f->pop3_deleted_count = 0;
-  
+
   mailpop3_do_uidl(f, msg_tab);
 
   return MAILPOP3_NO_ERROR;
@@ -726,7 +726,7 @@ mailpop3_get_content(mailpop3 * f, struct mailpop3_msg_info * msginfo,
       mmap_string_free(buffer);
       return MAILPOP3_ERROR_MEMORY;
     }
-    
+
     * result = result_multiline;
     * result_len = buffer->len;
     return MAILPOP3_NO_ERROR;
@@ -815,7 +815,7 @@ int mailpop3_dele(mailpop3 * f, unsigned int indx)
 
   msginfo->msg_deleted = TRUE;
   f->pop3_deleted_count ++;
-  
+
   return MAILPOP3_NO_ERROR;
 }
 
@@ -891,7 +891,7 @@ int mailpop3_capa(mailpop3 * f, clist ** result)
 
   if (r != RESPONSE_OK)
     return MAILPOP3_ERROR_CAPA_NOT_SUPPORTED;
-  
+
   capa_list = NULL;
   r = read_capa_resp(f, &capa_list);
   if (r != MAILPOP3_NO_ERROR)
@@ -926,7 +926,7 @@ int mailpop3_stls(mailpop3 * f)
 
   if (r != RESPONSE_OK)
     return MAILPOP3_ERROR_STLS_NOT_SUPPORTED;
-  
+
   return MAILPOP3_NO_ERROR;
 }
 
@@ -1007,7 +1007,7 @@ static char * cut_token(char * line)
 static int parse_response(mailpop3 * f, char * response)
 {
   char * msg;
-  
+
   if (response == NULL) {
     f->pop3_response = NULL;
     return RESPONSE_ERR;
@@ -1019,7 +1019,7 @@ static int parse_response(mailpop3 * f, char * response)
       msg = response + strlen(RESP_OK_STR) + 1;
     else
       msg = response + strlen(RESP_OK_STR);
-    
+
     if (mmap_string_assign(f->pop3_response_buffer, msg))
       f->pop3_response = f->pop3_response_buffer->str;
     else
@@ -1033,7 +1033,7 @@ static int parse_response(mailpop3 * f, char * response)
       msg = response + strlen(RESP_ERR_STR) + 1;
     else
       msg = response + strlen(RESP_ERR_STR);
-    
+
     if (mmap_string_assign(f->pop3_response_buffer, msg))
       f->pop3_response = f->pop3_response_buffer->str;
     else
@@ -1054,7 +1054,7 @@ static int parse_response(mailpop3 * f, char * response)
 static int parse_auth(mailpop3 * f, char * response)
 {
   char * msg;
-  
+
   if (response == NULL) {
     f->pop3_response = NULL;
     return RESPONSE_ERR;
@@ -1062,12 +1062,12 @@ static int parse_auth(mailpop3 * f, char * response)
 
   if ((strncmp(response, RESP_AUTH_CONT_STR, strlen(RESP_AUTH_CONT_STR)) == 0) &&
       (strncmp(response, RESP_OK_STR, strlen(RESP_OK_STR)) != 0)) {
-    
+
     if (response[strlen(RESP_AUTH_CONT_STR)] == ' ')
       msg = response + strlen(RESP_AUTH_CONT_STR) + 1;
     else
       msg = response + strlen(RESP_AUTH_CONT_STR);
-    
+
     if (mmap_string_assign(f->pop3_response_buffer, msg))
       f->pop3_response = f->pop3_response_buffer->str;
     else
@@ -1108,7 +1108,7 @@ static int read_list(mailpop3 * f, carray ** result)
       continue;
 
     size = strtol(line, &line, 10);
-    
+
     msg = mailpop3_msg_info_new(indx, size, NULL);
     if (msg == NULL)
       goto free_list;
@@ -1125,7 +1125,7 @@ static int read_list(mailpop3 * f, carray ** result)
   }
 
   * result = msg_tab;
-  
+
   return MAILPOP3_NO_ERROR;
 
  free_list:
@@ -1148,10 +1148,10 @@ static int read_uidl(mailpop3 * f, carray * msg_tab)
     line = read_line(f);
     if (line == NULL)
       goto err;
-    
+
     if (mailstream_is_end_multiline(line))
       break;
-    
+
     indx = strtol(line, &line, 10);
 
     if (!parse_space(&line))
@@ -1174,7 +1174,7 @@ static int read_uidl(mailpop3 * f, carray * msg_tab)
 
     msg->msg_uidl = uidl;
   }
-  
+
   return MAILPOP3_NO_ERROR;
 
  err:
@@ -1208,7 +1208,7 @@ static int read_capa_resp(mailpop3 * f, clist ** result)
       res = MAILPOP3_ERROR_STREAM;
       goto free_list;
     }
-    
+
     if (mailstream_is_end_multiline(line))
       break;
 
@@ -1256,7 +1256,7 @@ static int read_capa_resp(mailpop3 * f, clist ** result)
   }
 
   * result = list;
-  
+
   return MAILPOP3_NO_ERROR;
 
  free_param_list:
@@ -1314,9 +1314,9 @@ static int sasl_getsimple(void * context, int id,
     const char ** result, unsigned * len)
 {
   mailpop3 * session;
-  
+
   session = context;
-  
+
   switch (id) {
   case SASL_CB_USER:
     if (result != NULL)
@@ -1324,7 +1324,7 @@ static int sasl_getsimple(void * context, int id,
     if (len != NULL)
       * len = strlen(session->pop3_sasl.sasl_login);
     return SASL_OK;
-    
+
   case SASL_CB_AUTHNAME:
     if (result != NULL)
       * result = session->pop3_sasl.sasl_auth_name;
@@ -1332,24 +1332,24 @@ static int sasl_getsimple(void * context, int id,
       * len = strlen(session->pop3_sasl.sasl_auth_name);
     return SASL_OK;
   }
-  
+
   return SASL_FAIL;
 }
 
 static int sasl_getsecret(sasl_conn_t * conn, void * context, int id,
     sasl_secret_t ** psecret)
 {
+  UNUSED(conn);
   mailpop3 * session;
-  
   session = context;
-  
+
   switch (id) {
   case SASL_CB_PASS:
     if (psecret != NULL)
       * psecret = session->pop3_sasl.sasl_secret;
     return SASL_OK;
   }
-  
+
   return SASL_FAIL;
 }
 
@@ -1357,17 +1357,17 @@ static int sasl_getrealm(void * context, int id,
     const char ** availrealms,
     const char ** result)
 {
+  UNUSED(availrealms);
   mailpop3 * session;
-  
   session = context;
-  
+
   switch (id) {
   case SASL_CB_GETREALM:
     if (result != NULL)
       * result = session->pop3_sasl.sasl_realm;
     return SASL_OK;
   }
-  
+
   return SASL_FAIL;
 }
 #endif
@@ -1392,7 +1392,7 @@ int mailpop3_auth(mailpop3 * f, const char * auth_type,
   char * encoded;
   unsigned int encoded_len;
   unsigned int max_encoded;
-  
+
   sasl_callback[0].id = SASL_CB_GETREALM;
   sasl_callback[0].proc =  (int (*)(void)) sasl_getrealm;
   sasl_callback[0].context = f;
@@ -1401,14 +1401,14 @@ int mailpop3_auth(mailpop3 * f, const char * auth_type,
   sasl_callback[1].context = f;
   sasl_callback[2].id = SASL_CB_AUTHNAME;
   sasl_callback[2].proc =  (int (*)(void)) sasl_getsimple;
-  sasl_callback[2].context = f; 
+  sasl_callback[2].context = f;
   sasl_callback[3].id = SASL_CB_PASS;
   sasl_callback[3].proc =  (int (*)(void)) sasl_getsecret;
   sasl_callback[3].context = f;
   sasl_callback[4].id = SASL_CB_LIST_END;
   sasl_callback[4].proc =  NULL;
   sasl_callback[4].context = NULL;
-  
+
   len = strlen(password);
   secret = malloc(sizeof(* secret) + len);
   if (secret == NULL) {
@@ -1417,14 +1417,14 @@ int mailpop3_auth(mailpop3 * f, const char * auth_type,
   }
   secret->len = len;
   memcpy(secret->data, password, len + 1);
-  
+
   f->pop3_sasl.sasl_server_fqdn = server_fqdn;
   f->pop3_sasl.sasl_login = login;
   f->pop3_sasl.sasl_auth_name = auth_name;
   f->pop3_sasl.sasl_password = password;
   f->pop3_sasl.sasl_realm = realm;
   f->pop3_sasl.sasl_secret = secret;
-  
+
   /* init SASL */
   if (f->pop3_sasl.sasl_conn != NULL) {
     sasl_dispose((sasl_conn_t **) &f->pop3_sasl.sasl_conn);
@@ -1433,7 +1433,7 @@ int mailpop3_auth(mailpop3 * f, const char * auth_type,
   else {
     mailsasl_ref();
   }
-  
+
   r = sasl_client_new("pop", server_fqdn,
       local_ip_port, remote_ip_port, sasl_callback, 0,
       (sasl_conn_t **) &f->pop3_sasl.sasl_conn);
@@ -1441,38 +1441,38 @@ int mailpop3_auth(mailpop3 * f, const char * auth_type,
     res = MAILPOP3_ERROR_BAD_USER;
     goto free_secret;
   }
-  
+
   r = sasl_client_start(f->pop3_sasl.sasl_conn,
       auth_type, NULL, &sasl_out, &sasl_out_len, &mechusing);
   if ((r != SASL_CONTINUE) && (r != SASL_OK)) {
     res = MAILPOP3_ERROR_BAD_USER;
     goto free_sasl_conn;
   }
-  
+
   snprintf(command, POP3_STRING_SIZE, "AUTH %s\r\n", auth_type);
-  
+
   r = send_command(f, command);
   if (r == -1) {
     res = MAILPOP3_ERROR_STREAM;
     goto free_sasl_conn;
   }
-  
+
   while (1) {
     char * response;
-    
+
     response = read_line(f);
-    
+
     r = parse_auth(f, response);
     switch (r) {
     case RESPONSE_OK:
       f->pop3_state = POP3_STATE_TRANSACTION;
       res = MAILPOP3_NO_ERROR;
       goto free_sasl_conn;
-      
+
     case RESPONSE_ERR:
       res = MAILPOP3_ERROR_BAD_USER;
       goto free_sasl_conn;
-    
+
     case RESPONSE_AUTH_CONT:
       {
         size_t response_len;
@@ -1480,14 +1480,14 @@ int mailpop3_auth(mailpop3 * f, const char * auth_type,
         unsigned int decoded_len;
         unsigned int max_decoded;
         int got_response;
-        
+
         got_response = 1;
         if (* f->pop3_response == '\0')
           got_response = 0;
-        
+
         if (got_response) {
           char * p;
-          
+
           p = strchr(f->pop3_response, '\r');
           if (p != NULL) {
             * p = '\0';
@@ -1503,34 +1503,34 @@ int mailpop3_auth(mailpop3 * f, const char * auth_type,
             res = MAILPOP3_ERROR_MEMORY;
             goto free_sasl_conn;
           }
-          
+
           r = sasl_decode64(f->pop3_response, response_len,
               decoded, max_decoded + 1, &decoded_len);
-          
+
           if (r != SASL_OK) {
             free(decoded);
             res = MAILPOP3_ERROR_MEMORY;
             goto free_sasl_conn;
           }
-          
+
           r = sasl_client_step(f->pop3_sasl.sasl_conn,
               decoded, decoded_len, NULL, &sasl_out, &sasl_out_len);
-          
+
           free(decoded);
-          
+
           if ((r != SASL_CONTINUE) && (r != SASL_OK)) {
             res = MAILPOP3_ERROR_BAD_USER;
             goto free_sasl_conn;
           }
         }
-        
+
         max_encoded = ((sasl_out_len + 2) / 3) * 4;
         encoded = malloc(max_encoded + 1);
         if (encoded == NULL) {
           res = MAILPOP3_ERROR_MEMORY;
           goto free_sasl_conn;
         }
-        
+
         r = sasl_encode64(sasl_out, sasl_out_len,
             encoded, max_encoded + 1, &encoded_len);
         if (r != SASL_OK) {
@@ -1538,12 +1538,12 @@ int mailpop3_auth(mailpop3 * f, const char * auth_type,
           res = MAILPOP3_ERROR_MEMORY;
           goto free_sasl_conn;
         }
-        
+
         snprintf(command, POP3_STRING_SIZE, "%s\r\n", encoded);
         r = send_command(f, command);
-        
+
         free(encoded);
-        
+
         if (r == -1) {
           res = MAILPOP3_ERROR_STREAM;
           goto free_sasl_conn;
@@ -1555,7 +1555,7 @@ int mailpop3_auth(mailpop3 * f, const char * auth_type,
 
   f->pop3_state = POP3_STATE_TRANSACTION;
   res = MAILPOP3_NO_ERROR;
-  
+
  free_sasl_conn:
   sasl_dispose((sasl_conn_t **) &f->pop3_sasl.sasl_conn);
   f->pop3_sasl.sasl_conn = NULL;
