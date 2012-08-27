59471e91b22c55f755502c997929f4e40e28409f
lysannkessler <lysann.kessler@gmail.com>
2012-08-27 20:56:03 +0200

    autodiscover test server: improved error response

--- tests/low-level/oxws/test_server/autodiscover/autodiscover.rb
+++ tests/low-level/oxws/test_server/autodiscover/autodiscover.rb
@@ -13,20 +13,40 @@ class Autodiscover < Sinatra::Base
 
   set :nokogiri, :views => File.dirname(__FILE__)
 
-  post '/autodiscover/autodiscover.xml', :provides => :xml do
-    request_document = Nokogiri::XML.parse request.body
-    email_address = request_document.xpath('/xmlns:Autodiscover/xmlns:Request/xmlns:EMailAddress').text
+  @@route = '/autodiscover/autodiscover.xml'
+
+  get @@route, :provides => :xml do
+    return error_invalid_request
+  end
+
+  post @@route, :provides => :xml do
+    begin
+      request_document = Nokogiri::XML.parse request.body
+      email_address_node = request_document.xpath('/xmlns:Autodiscover/xmlns:Request/xmlns:EMailAddress')
+      raise 'Missing EMailAddress' if email_address_node.empty?
+      email_address = email_address_node.text
+    rescue Exception => e
+      return error_invalid_request
+    end
 
     user_info = self.class.users.find { |user| user['email'] == email_address }
     if user_info
-      return nokogiri :success, :locals => {
+      nokogiri :success, :locals => {
         :user_info => user_info,
         :host => request.host,
         :host_with_port => request.host_with_port
       }
     else
-      return nokogiri :error
+      error(500, 'The e-mail address cannot be found.')
     end
+  end
+
+  def error_invalid_request
+    error(600, 'Invalid Request')
+  end
+
+  def error(code, message)
+    nokogiri :error, :locals => { :error_code => code, :message => message }
   end
 end
 
