ee5bac8b5930eb3ee83c5f4b0522140d9a25a02f
lysannkessler <lysann.kessler@gmail.com>
2012-09-15 23:58:11 +0200

    add test list of items to config dir

--- tests/low-level/oxws/test_server/config/config.rb
+++ tests/low-level/oxws/test_server/config/config.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'yaml'
+require 'date'
 
 module LibetpanTest
 module OXWS
@@ -11,6 +12,29 @@ module Config
 
   def self.users
     @@users ||= self.load_yaml 'users'
+  end
+
+  @@items_per_folder = nil
+  def self.items
+    if @@items_per_folder.nil?
+      @@items_per_folder = self.load_yaml 'items'
+      @@items_per_folder.each do |name, items|
+        items.each { |item| self.convert_item item }
+      end
+    end
+    @@items_per_folder
+  end
+
+  private
+
+  def self.convert_item(item)
+    item['DateTimeSent'] = self.convert_datetime(item['DateTimeSent'])
+    item['DateTimeCreated'] = self.convert_datetime(item['DateTimeCreated'])
+    return item
+  end
+
+  def self.convert_datetime(string)
+    return DateTime.parse(string)
   end
 end
 
