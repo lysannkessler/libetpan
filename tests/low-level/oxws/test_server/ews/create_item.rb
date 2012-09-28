146c7ecff6d02757b496be864ca66930459fab86
lysannkessler <lysann.kessler@gmail.com>
2012-09-28 18:01:20 +0100

    create_item returns item ids only for SaveOnly disposition

--- tests/low-level/oxws/test_server/ews/create_item.rb
+++ tests/low-level/oxws/test_server/ews/create_item.rb
@@ -10,14 +10,15 @@ class CreateItem
   end
 
   def handle(message_node)
-    items = [
-      { 'class_id' => 'message',
+    items = []
+    if message_node['MessageDisposition'] == 'SaveOnly'
+      items << { 'class_id' => 'message',
         'ItemId' => {
           'Id' => 'AAAhAEx5c2Fubi5LZXNzbGVyQGhwaS51bmktcG90c2RhbS5kZQBGAAAAAAAOQP8UchHYRYW2kLdtztl9BwDi0v2At9wNQ4mCHb9pSDgCAAAAe6dBAACac5yph9kYRpDIsFsCUOP+ACTMRrcJAAA=',
           'ChangeKey' => 'CQAAABYAAACac5yph9kYRpDIsFsCUOP+ACTMRrsJ'
         }
       }
-    ]
+    end
     @ews.render_response :create_item, :locals => { :items => items }
   end
 end
